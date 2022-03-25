fa97e14c9ffd90fd95e8e439e89a1c4ae91f67a3
Fafnir <79815557+FihFafnir@users.noreply.github.com>
2022-03-25 00:49:38 -0300

    Update main.js
    Code has been optimized and cleaned.

--- main.js
+++ main.js
@@ -1,245 +1,81 @@
-let inputs = document.querySelectorAll(".input")
-function encoder(alphabet) {
-  let separateText = alphabet.normalize("NFD").toLowerCase().split("")
-  let codedText = []
-  for (let i = 0; i < separateText.length; i++) {
-    switch (separateText[i]) {
-      case 'a':
-        codedText.push(".-")
-        break;
-      case 'b':
-        codedText.push("-...")
-        break;
-      case 'c':
-        codedText.push("-.-.")
-        break;
-      case 'd':
-        codedText.push("-..")
-        break;
-      case 'e':
-        codedText.push(".")
-        break;
-      case 'f':
-        codedText.push("..-.")
-        break;
-      case 'g':
-        codedText.push("--.")
-        break;
-      case 'h':
-        codedText.push("....")
-        break;
-      case 'i':
-        codedText.push("..")
-        break;
-      case 'j':
-        codedText.push(".---")
-        break;
-      case 'k':
-        codedText.push("-.-")
-        break;
-      case 'l':
-        codedText.push(".-..")
-        break;
-      case 'm':
-        codedText.push("--")
-        break;
-      case 'n':
-        codedText.push("-.")
-        break;
-      case 'o':
-        codedText.push("---")
-        break;
-      case 'p':
-        codedText.push(".--.")
-        break;
-      case 'q':
-        codedText.push("--.-")
-        break;
-      case 'r':
-        codedText.push(".-.")
-        break;
-      case 's':
-        codedText.push("...")
-        break;
-      case 't':
-        codedText.push("-")
-        break;
-      case 'u':
-        codedText.push("..-")
-        break;
-      case 'v':
-        codedText.push("...-")
-        break;
-      case 'w':
-        codedText.push(".--")
-        break;
-      case 'x':
-        codedText.push("-..-")
-        break;
-      case 'y':
-        codedText.push("-.--")
-        break;
-      case 'z':
-        codedText.push("--..")
-        break;
-      case '1':
-        codedText.push(".----")
-        break;
-      case '2':
-        codedText.push("..---")
-        break;
-      case '3':
-        codedText.push("...--")
-        break;
-      case '4':
-        codedText.push("....-")
-        break;
-      case '5':
-        codedText.push(".....")
-        break;
-      case '6':
-        codedText.push("-....")
-        break;
-      case '7':
-        codedText.push("--...")
-        break;
-      case '8':
-        codedText.push("---..")
-        break;
-      case '9':
-        codedText.push("----.")
-        break;
-      case '0':
-        codedText.push("-----")
-        break;
+const encoderInput = document.querySelector("#encoder");
+const decoderInput = document.querySelector("#decoder");
+
+const morseCodeList = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  " ": ""
+}
+
+function encoder(text) {
+  const letters = text.normalize("NFD").toLowerCase().split("");
+  const codedLetters = [];
+
+  for (let letter of letters) {
+    const morseCode = morseCodeList[letter]; 
+    codedLetters.push(morseCode);
+  }
+
+  const codedText = codedLetters.join("/");
+  writeResult(codedText, decoderInput);
+}
+
+function decoder(morseCode) {
+  const separateMorseCode = morseCode.split("/");
+  const decodedLetters = [];
+
+  const getTheLetterByTheMorseCode = (morseCode) => {
+    for(let letter in morseCodeList) {
+      if(morseCodeList[letter] === morseCode && morseCodeList.hasOwnProperty(letter)) return letter;
     }
   }
-  writeResult(codedText, 1)
-}
-function decoder(morse) {
-  let separateText = morse.split("/")
-  let decodedText = []
-  for (let i = 0; i < separateText.length; i++) {
-    switch (separateText[i]) {
-      case '.-':
-        decodedText.push("a")
-        break;
-      case '-...':
-        decodedText.push("b")
-        break;
-      case '-.-.':
-        decodedText.push("c")
-        break;
-      case '-..':
-        decodedText.push("d")
-        break;
-      case '.':
-        decodedText.push("e")
-        break;
-      case '..-.':
-        decodedText.push("f")
-        break;
-      case '--.':
-        decodedText.push("g")
-        break;
-      case '....':
-        decodedText.push("h")
-        break;
-      case '..':
-        decodedText.push("i")
-        break;
-      case '.---':
-        decodedText.push("j")
-        break;
-      case '-.-':
-        decodedText.push("k")
-        break;
-      case '.-..':
-        decodedText.push("l")
-        break;
-      case '--':
-        decodedText.push("m")
-        break;
-      case '-.':
-        decodedText.push("n")
-        break;
-      case '---':
-        decodedText.push("o")
-        break;
-      case '.--.':
-        decodedText.push("p")
-        break;
-      case '--.-':
-        decodedText.push("q")
-        break;
-      case '.-.':
-        decodedText.push("r")
-        break;
-      case '...':
-        decodedText.push("s")
-        break;
-      case '-':
-        decodedText.push("t")
-        break;
-      case '..-':
-        decodedText.push("u")
-        break;
-      case '...-':
-        decodedText.push("v")
-        break;
-      case '.--':
-        decodedText.push("w")
-        break;
-      case '-..-':
-        decodedText.push("x")
-        break;
-      case '-.--':
-        decodedText.push("y")
-        break;
-      case '--..':
-        decodedText.push("z")
-        break;
-      case '.----':
-        decodedText.push("1")
-        break;
-      case '..---':
-        decodedText.push("2")
-        break;
-      case '...--':
-        decodedText.push("3")
-        break;
-      case '....-':
-        decodedText.push("4")
-        break;
-      case '.....':
-        decodedText.push("5")
-        break;
-      case '-....':
-        decodedText.push("6")
-        break;
-      case '--...':
-        decodedText.push("7")
-        break;
-      case '---..':
-        decodedText.push("8")
-        break;
-      case '----.':
-        decodedText.push("9")
-        break;
-      case '-----':
-        decodedText.push("0")
-        break;
-    }
+
+  for (let morse of separateMorseCode) {
+    const letter = getTheLetterByTheMorseCode(morse);
+    if(letter !== undefined) decodedLetters.push(letter);
   }
-  writeResult(decodedText, 0)
+
+  const decodedText = decodedLetters.join("");
+  writeResult(decodedText, encoderInput);
 }
-function writeResult(txt, number) {
-  inputs[number].innerHTML = ""
-  for(let i = 0; i < txt.length; i++) {
-    if(number == 1) {
-      inputs[number].innerHTML += `${txt[i]}/`  
-    } else {
-      inputs[number].innerHTML += `${txt[i]} `
-    } 
-  }
+
+function writeResult(text, input) {
+  input.innerText = text;
 }
+
+encoderInput.onkeyup = () => encoder(event.target.innerText);
+decoderInput.onkeyup = () => decoder(event.target.innerText);
