66f18d3c4c07a3016d5cc12fc6d6103d82bf020b
FihFafnir <fihfdragon@gmail.com>
2023-03-18 17:29:16 -0300

    Update

--- main.js
+++ main.js
@@ -1,81 +1,84 @@
+const inputs = document.querySelectorAll(".input");
 const encoderInput = document.querySelector("#encoder");
 const decoderInput = document.querySelector("#decoder");
 
 const morseCodeList = {
-  a: ".-",
-  b: "-...",
-  c: "-.-.",
-  d: "-..",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-  m: "--",
-  n: "-.",
-  o: "---",
-  p: ".--.",
-  q: "--.-",
-  r: ".-.",
-  s: "...",
-  t: "-",
-  u: "..-",
-  v: "...-",
-  w: ".--",
-  x: "-..-",
-  y: "-.--",
-  z: "--..",
-  "0": "-----",
-  "1": ".----",
-  "2": "..---",
-  "3": "...--",
-  "4": "....-",
-  "5": ".....",
-  "6": "-....",
-  "7": "--...",
-  "8": "---..",
-  "9": "----.",
-  " ": ""
-}
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    " ": "/",
+};
 
-function encoder(text) {
-  const letters = text.normalize("NFD").toLowerCase().split("");
-  const codedLetters = [];
+const funcs = {
+    encoder: (text) => {
+        const letters = text.normalize("NFD").toLowerCase().split("");
 
-  for (let letter of letters) {
-    const morseCode = morseCodeList[letter]; 
-    codedLetters.push(morseCode);
-  }
+        const codedLetters = letters.map((letter) => {
+            const morseCode = morseCodeList[letter];
+            return morseCode || "";
+        });
 
-  const codedText = codedLetters.join("/");
-  writeResult(codedText, decoderInput);
-}
+        const codedText = codedLetters.join(" ");
+        writeResult(codedText, decoderInput);
+    },
+    decoder: (morseCode) => {
+        const separateMorseCode = morseCode.split(" ");
 
-function decoder(morseCode) {
-  const separateMorseCode = morseCode.split("/");
-  const decodedLetters = [];
+        const getTheLetterByTheMorseCode = (morseCode) => {
+            return Object.keys(morseCodeList).find(
+                (letter) => morseCodeList[letter] === morseCode
+            );
+        };
 
-  const getTheLetterByTheMorseCode = (morseCode) => {
-    for(let letter in morseCodeList) {
-      if(morseCodeList[letter] === morseCode && morseCodeList.hasOwnProperty(letter)) return letter;
-    }
-  }
+        const decodedLetters = separateMorseCode.map((morseCode) => {
+            return getTheLetterByTheMorseCode(morseCode);
+        });
 
-  for (let morse of separateMorseCode) {
-    const letter = getTheLetterByTheMorseCode(morse);
-    if(letter !== undefined) decodedLetters.push(letter);
-  }
+        const decodedText = decodedLetters.join("");
+        writeResult(decodedText, encoderInput);
+    },
+};
 
-  const decodedText = decodedLetters.join("");
-  writeResult(decodedText, encoderInput);
-}
+const writeResult = (text, input) => {
+    input.textContent = text;
+};
 
-function writeResult(text, input) {
-  input.innerText = text;
-}
-
-encoderInput.onkeyup = () => encoder(event.target.innerText);
-decoderInput.onkeyup = () => decoder(event.target.innerText);
+inputs.forEach((input) => {
+    input.onkeyup = input.onkeydown = () => {
+        const currentFunc = funcs[input.id];
+        currentFunc(input.innerText);
+    };
+});
